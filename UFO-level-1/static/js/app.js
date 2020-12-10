5df8d33d971dec6a42ab1dd33835cbe308d9f012
Veronica Nixon <vsnixon@gmail.com>
2020-12-09 20:53:44 -0700

    Adds skeleton code for appending table rows and elements dynamically.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,33 @@
-// from data.js
+// Import data from data.js.
 var tableData = data;
+var arrayToDisplay = [];
 
-// YOUR CODE HERE!
+// function selectByDate(sighting, date) {
+//    if (sighting.datetime === date)
+//      {
+//          return sighting object;
+//      };
+// }
+
+// for (var i: 0, var i: tableData.length, i++)
+//   {
+//      sighting object = selectByDate(tableData[i], date);
+//       arrayToDisplay.append(sighting object);
+//   }
+
+// Append table body to table.
+var tableBody = d3.select('tbody');
+
+// Append a row to table body.
+d3.select(tableBody).append('tr');
+
+// Append 7 cells to table row.
+d3.select(tableRow).append('td');
+d3.select(tableRow).append('td');
+d3.select(tableRow).append('td');
+d3.select(tableRow).append('td');
+d3.select(tableRow).append('td');
+d3.select(tableRow).append('td');
+d3.select(tableRow).append('td');
+
+
